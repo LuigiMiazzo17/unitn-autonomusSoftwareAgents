6271217b9eb21ccd1d468229cafc4d18d7b65488
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2025-08-27 21:26:39 +0200

    feat: anytime planner

--- pddl/solver.py
+++ pddl/solver.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from pathlib import Path
 
+from datetime import datetime
 from flask import Flask, jsonify, request
 from unified_planning.io import PDDLReader
-from unified_planning.shortcuts import OneshotPlanner
+from unified_planning.shortcuts import AnytimePlanner
+
 
 app = Flask(__name__)
 reader = PDDLReader()
@@ -12,20 +14,32 @@ domain = (Path(__file__).parent / "domain.pddl").read_text()
 
 @app.route("/solve", methods=["POST"])
 def solve_route():
-    try:
-        problem = request.get_json().get("problem")
+    # try:
+    problem = request.get_json().get("problem")
 
-        problem = reader.parse_problem_string(domain, problem)
+    start = datetime.now()
+    problem = reader.parse_problem_string(domain, problem)
 
-        with OneshotPlanner(name="aries") as planner:
-            result = planner.solve(problem)
-            plan = result.plan
+    timeout = request.get_json().get("timeout", 1000)
+    plan = None
+    with AnytimePlanner(
+        problem_kind=problem.kind, anytime_guarantee="INCREASING_QUALITY"
+    ) as planner:
+        for p in planner.get_solutions(problem, timeout=timeout):
+            if p.plan:
+                plan = p.plan
+    print("Time taken:", datetime.now() - start)
 
+    if plan:
         return jsonify(
             {"status": "success", "plan": [str(action) for action in plan.actions]}
         )
-    except Exception as e:
-        return jsonify({"status": "error", "msg": str(e)}), 500
+    else:
+        return jsonify(
+            {"status": "error", "msg": "No plan found within the time limit"}
+        ), 404
+    # except Exception as e:
+    #     return jsonify({"status": "error", "msg": str(e)}), 500
 
 
 if __name__ == "__main__":
