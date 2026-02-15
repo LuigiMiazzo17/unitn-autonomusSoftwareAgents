5607cedcb37b75f404a02759fe07bff891ef6fea
Miazzo Luigi <17008163+LuigiMiazzo17@users.noreply.github.com>
2026-02-15 22:51:01 +0100

    chore: remove unused function

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,22 +1,5 @@
 import { Position } from "src/beliefs/types";
 
-// Source - https://stackoverflow.com/a/47593316
-// Posted by bryc, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-02-15, License - CC BY-SA 4.0
-export function splitmix32(a: number) {
-  a = ((1 / a) * 2 ** 32) >>> 0;
-
-  return function () {
-    a |= 0;
-    a = (a + 0x9e3779b9) | 0;
-    let t = a ^ (a >>> 16);
-    t = Math.imul(t, 0x21f0aaad);
-    t = t ^ (t >>> 15);
-    t = Math.imul(t, 0x735a2d97);
-    return ((t = t ^ (t >>> 15)) >>> 0) / 4294967296;
-  };
-}
-
 export function normalizePos(pos: Position): Position {
   return { x: Math.floor(pos.x), y: Math.floor(pos.y) };
 }
